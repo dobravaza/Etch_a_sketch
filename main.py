9f161ba5ea9fdeca9b94cb293503abb1a96d0239
Grzegorz <97113127+dobravaza@users.noreply.github.com>
2023-09-12 15:37:19 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,39 @@
 from turtle import Turtle, Screen
 
-tim = Turtle()
+bill = Turtle()
 screen = Screen()
 
 
+# direction functions
 def move_forwards():
-    tim.forward(10)
+    bill.forward(10)
 
 
+def move_backwards():
+    bill.backward(10)
+
+
+def move_clockwise():
+    bill.right(10)
+
+
+def move_counter_clockwise():
+    bill.left(10)
+
+
+def clear():
+    bill.clear()
+    bill.reset()
+
+
+# event listener
 screen.listen()
-screen.onkey(key="space", fun=move_forwards)
+
+# controls
+screen.onkey(key="w", fun=move_forwards)
+screen.onkey(key="s", fun=move_backwards)
+screen.onkey(key="a", fun=move_counter_clockwise)
+screen.onkey(key="d", fun=move_clockwise)
+screen.onkey(key="c", fun=clear)
+
 screen.exitonclick()
